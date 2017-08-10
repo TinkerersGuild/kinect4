00d7f07edd25d0c7b34cddca2d2849c726f455ad
jai <tyggerjai@gmail.com>
2017-08-10 13:15:24 +1000

    Pics!

--- tests/test_Board.py
+++ tests/test_Board.py
@@ -1,8 +1,10 @@
 import sys
+import pytest
 sys.path.append("../lib/")
 import board as b
 
 
 test_tiles = ([12,13], [11,37],[22,11], [13,24],  [13,48], [25,24])
 print("Input: {}".format(test_tiles))
-myboard = b.Board(test_tiles, 4,4)
+myboard = b.Board(4,4)
+myboard.setTiles(test_tiles)

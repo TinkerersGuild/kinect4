ba65798cc0da9fe6d991ad1743ffa9793266a984
jai <solitaire@tygger.net>
2018-02-09 14:37:32 +1100

    Better colour changing

--- src/esp/neo1/neo1.ino
+++ src/esp/neo1/neo1.ino
@@ -4,9 +4,9 @@
 #include <Adafruit_NeoPixel.h>
 #include <ESP8266WiFi.h>
 
-const char* ssid = "pride";
-const char* password = "edirppride";
-const char* gameserver = "192.168.8.206";
+const char* ssid = "lynx";
+const char* password = "FishFish";
+const char* gameserver = "192.168.1.112";
 int gameport = 5000;
 // Create an instance of the server
 // specify the port to listen on as an argument
@@ -24,32 +24,38 @@ WiFiServer server(80);
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+ 
 int RED = pixels.Color(150,0,0);
 int GREEN = pixels.Color(0,150,0);
 int BLUE = pixels.Color(0, 0, 150);
+int YELLOW = pixels.Color(150, 150, 0);
+
+int myColors[4] = {RED, GREEN, BLUE, YELLOW};
 
 int delayval = 500; // delay for half a second
 int colchange = 0;
 int col = RED;
 
-int register(){
+
+int reg_client(){
   WiFiClient client;
 
 
-if (client.connect(gameserver, gameport)) {
+  if (client.connect(gameserver, gameport)) {
     
 
-   String s = "GET /register?ID=";
-   s += WiFi.macAddress();
+    String s = "GET /register?ID=";
+    s += WiFi.macAddress();
     client.println(s);
     
     return 1;
    
    }
-   return 0;
+  return 0;
 
 
 }
+
 void setup() {
 
   pixels.begin(); // This initializes the NeoPixel library.
@@ -67,7 +73,7 @@ void setup() {
     
   }
   
-  if (register()){
+  if (reg_client()){
     pixels.setPixelColor(1, GREEN); // Moderately bright green color.
     pixels.show(); // This sends the updated pixel color to the hardware.
   }
@@ -76,7 +82,6 @@ void setup() {
 
 void loop() {
 
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
  WiFiClient client = server.available();
   if (!client) {
     return;
@@ -90,19 +95,21 @@ void loop() {
   
   // Read the first line of the request
   String req = client.readStringUntil('\r');
+
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nOk, set ";
+  s += req;
   client.flush();
 
 
-  if (req.indexOf("/col/R") != -1){
-    col = RED;
-    colchange = 1;
-    
-  }else if (req.indexOf("/col/G") != -1){
-    col = GREEN;
-    colchange = 1;
-
-  }else if (req.indexOf("/col/B") != -1){
-    col = BLUE;
+// Request is formatted for a URL, but it's "telnet compatible"
+  int colpos = req.indexOf("/col/");
+  if ( colpos != -1){
+    s += "<br/> New color at ";
+    s += colpos;
+    int newcol = req.charAt(colpos + 5) - 48;
+    s += newcol;
+    s += "<br/>";
+    col = myColors[newcol];
     colchange = 1;
 
 
@@ -124,11 +131,12 @@ void loop() {
     
   
  
-  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nOk, set ";
-  s += req;
   s += "</html>\n";
 
   // Send the response to the client
   client.print(s);
   delay(1);
 }
+
+
+
--- src/flaskserver/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-stuff = list()
-
-from app import routes
